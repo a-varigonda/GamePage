dff6554923eb254d4015b5994349b14b3394d7ef
Ankita Varigonda <avarigonda18@gmail.com>
2025-12-23 19:12:31 -0600

    toggle mode

--- gamepage/src/components/NavBar.tsx
+++ gamepage/src/components/NavBar.tsx
@@ -1,12 +1,14 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image, Switch, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import { ColorModeButton } from "../components/ui/color-mode";
 
 function NavBar() {
   return (
     <>
-      <HStack>
+      <HStack justifyContent="space-between" padding="10px">
         <Image src={logo} boxSize="60px"></Image>
         <Text>NavBar</Text>
+        <ColorModeButton />
       </HStack>
     </>
   );
