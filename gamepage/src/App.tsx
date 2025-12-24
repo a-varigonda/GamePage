c2a4411255edbc2e692d412197432686ce512b44
Ankita Varigonda <avarigonda18@gmail.com>
2025-12-23 18:48:19 -0600

    responsive layout

--- gamepage/src/App.tsx
+++ gamepage/src/App.tsx
@@ -1,9 +1,26 @@
-function App() {
-  const Demo = () => {
-    return <></>;
-  };
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
-  return <>{Demo()}</>;
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        nav
+      </GridItem>
+
+      <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
+        aside
+      </GridItem>
+
+      <GridItem area="main" bg="dodgerblue">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
