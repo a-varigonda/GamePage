c1a5adc5ee1c54ff08e9054862723f4395a224f9
Ankita Varigonda <avarigonda18@gmail.com>
2025-12-27 20:50:43 -0600

    fetch genres

--- gamepage/src/App.tsx
+++ gamepage/src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GeneList";
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
       </GridItem>
 
       <GridItem area="aside" display={{ base: "none", lg: "block" }}>
-        aside
+        <GenreList />
       </GridItem>
 
       <GridItem area="main">
