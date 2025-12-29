9fb0e7d10e70a246eca38b6087954dd6f6f5a44a
Ankita Varigonda <avarigonda18@gmail.com>
2025-12-29 14:13:58 -0600

    sorting functionality

--- gamepage/src/components/SortSelector.tsx
+++ gamepage/src/components/SortSelector.tsx
@@ -1,23 +1,44 @@
 import { Button, Menu, Portal } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-function SortSelector() {
+interface Props {
+  sortOrder: string;
+  onSelectSortOrder: (sortOrder: string) => void;
+}
+
+function SortSelector({ sortOrder, onSelectSortOrder }: Props) {
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date Added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release Date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average Rating" },
+  ];
+
+  const currentSortOrder = sortOrders.find(
+    (order) => order.value === sortOrder
+  );
+
   return (
     <Menu.Root>
       <Menu.Trigger asChild>
         <Button variant="subtle" size="sm" fontSize="md" borderRadius={8}>
-          Order <BsChevronDown />
+          Order by: {currentSortOrder?.label || "Relevance"} <BsChevronDown />
         </Button>
       </Menu.Trigger>
       <Portal>
         <Menu.Positioner>
           <Menu.Content>
-            <Menu.Item value="a">Relevance</Menu.Item>
-            <Menu.Item value="a">Date Added</Menu.Item>
-            <Menu.Item value="a">Name</Menu.Item>
-            <Menu.Item value="a">Release Date</Menu.Item>
-            <Menu.Item value="a">Popularity</Menu.Item>
-            <Menu.Item value="a">Average Rating</Menu.Item>
+            {sortOrders.map((order) => (
+              <Menu.Item
+                onClick={() => onSelectSortOrder(order.value)}
+                key={order.value}
+                value={order.value}
+              >
+                {order.label}
+              </Menu.Item>
+            ))}
           </Menu.Content>
         </Menu.Positioner>
       </Portal>
